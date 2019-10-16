a6e2c51d03ce2a8804fcd6252236071de0fab271
g9dragon <52151268+g9dragon@users.noreply.github.com>
2019-10-16 18:31:43 +0700

    Update getpagecontent.py

--- getpagecontent.py
+++ getpagecontent.py
@@ -15,9 +15,11 @@ def getcontent(pageurl):
     particle['content']=psoup.p.text.strip()
     for ctn in psoup.article.find_all('p',{"class":"Normal"}):
         particle['content']+=ctn.text.strip()
+    #Search for Call CmtFile Cmd
     h=str(psoup.find_all('div',{"id":"box_comment_vne"}))
     bstr=h.find('"article_id"')
     estr=h.find(',"limit"')
+    #Transform CmtFile URL
     myurl='https://usi-saas.vnexpress.net/index/get?callback=jQuery&offset=0&'+h[bstr:estr].replace("article","object").replace("_","").replace('"','').replace(':','=').replace(',','&')
     jsfile=urllib.request.urlopen(myurl)
     jsfct=jsfile.read().decode('utf8').replace(');','').replace("/**/ typeof jQuery === 'function' && jQuery(","")
